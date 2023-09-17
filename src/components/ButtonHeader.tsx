b28983969f2459cf28d138742b87d88602aaf6b6
guyddogl <guyddogl@gmail.com>
2023-09-16 22:49:11 -0300

    Disable prop

--- src/components/ButtonHeader.tsx
+++ src/components/ButtonHeader.tsx
@@ -5,13 +5,14 @@ interface IButtonHeader {
   variant: string, 
   icon: React.ReactElement, 
   matches: boolean,
+  disabled: boolean,
 }
 
-export function ButtonHeader({text, variant, icon, matches}: IButtonHeader) {
+export function ButtonHeader({text, variant, icon, matches, disabled}: IButtonHeader) {
 
   return (
     <Tooltip label={text} color="#228be6" withArrow>
-      <Button variant={variant} leftIcon={!matches && icon} style={{minWidth: `${matches ? '0px' : '115px'}`}}>
+      <Button variant={variant} leftIcon={!matches && icon} style={{minWidth: `${matches ? '0px' : '115px'}`}} disabled={disabled}>
         {matches ? icon : text}
       </Button>
     </Tooltip>
