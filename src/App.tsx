25ae2ba492cb6e6cf551b86cef68bc87c7d2d4e9
guyddogl <guyddogl@gmail.com>
2023-09-16 23:28:08 -0300

    Loading overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
+import { Container, Center, Loader } from '@mantine/core';
 import { Header, Footer } from './components'
+import { useMediaQuery } from '@mantine/hooks';
 
 function App() {
-  const api = import.meta.env.VITE_API_KEY;
-  console.log(api);
+  // const api = import.meta.env.VITE_API_KEY;
+  const matches = useMediaQuery('(max-width: 790px)');
+  const matchesXS = useMediaQuery('(max-width: 510px)');
 
   return (
     <>
-      <Header />
-      <Footer />
+      {matches === undefined || matchesXS === undefined ? (
+        <Container size="xs">
+          <Center style={{ minHeight: '90vh'}}>
+            <Loader />
+          </Center>
+        </Container>
+      ) : (
+        <>
+          <Header matches={matches} matchesXS={matchesXS} />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
 
-export default App
+export default App;
