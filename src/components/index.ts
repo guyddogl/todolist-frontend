0e239d9d6ae4b77246b4c5a9b9d4eb7d9a05625b
guyddogl <guyddogl@gmail.com>
2023-09-17 21:08:15 -0300

    Checkbox component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,12 @@
 import { ButtonHeader } from './ButtonHeader';
+import { Checkbox } from './Checkbox';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { ScrollToTop } from './ScrollToTop';
 
 export {
   ButtonHeader,
+  Checkbox,
   Header,
   Footer,
   ScrollToTop
